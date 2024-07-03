c7e2e709910e21237677e791ae9e9fa20fb0f2e7
Nikolai Karpenko <fenyx69@mail.ru>
2024-07-03 15:19:52 +0300

    Akkerman

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿int Akerman(int x, int y)
+{
+    if (x == 0)
+    {
+        return y + 1;
+    }
+
+    else if ( x > 0 && y == 0)
+    {
+        return Akerman(x - 1, 1);
+    }
+
+    else if (x > 0 && y > 0)
+    {
+        return Akerman(x - 1, Akerman(x, y - 1));
+    }
+
+    else 
+    {
+        return 0;
+    }
+}
+
+int m = 3;
+int n = 2;
+
+int function = Akerman(m, n);
+
+System.Console.WriteLine(function);
