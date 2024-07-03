41f3bb83396835fae134d13a0d66e5ae207478ef
Nikolai Karpenko <fenyx69@mail.ru>
2024-07-03 15:52:28 +0300

    два задания

--- Program.cs
+++ Program.cs
@@ -1,29 +1,68 @@
-﻿int Akerman(int x, int y)
-{
-    if (x == 0)
-    {
-        return y + 1;
-    }
+﻿
 
-    else if ( x > 0 && y == 0)
-    {
-        return Akerman(x - 1, 1);
-    }
+//// Задание 1
 
-    else if (x > 0 && y > 0)
-    {
-        return Akerman(x - 1, Akerman(x, y - 1));
-    }
+// void Counter(int m, int n)
+// {
+//     if (m > n)
+//     {
+//         return;
+//     }
+//     System.Console.Write($"{m} ");
+//     Counter(m + 1, n);
+// }
 
-    else 
-    {
-        return 0;
-    }
-}
 
-int m = 3;
-int n = 2;
+// Console.Write("Введите значение M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-int function = Akerman(m, n);
+// m += 1;
+// n -= 1;
 
-System.Console.WriteLine(function);
+// Counter(m, n);
+
+
+
+
+///// Задание 2
+
+
+// int Akkerman(int x, int y)
+// {
+//     if (x == 0)
+//     {
+//         return y + 1;
+//     }
+
+//     else if ( x > 0 && y == 0)
+//     {
+//         return Akkerman(x - 1, 1);
+//     }
+
+//     else if (x > 0 && y > 0)
+//     {
+//         return Akkerman(x - 1, Akkerman(x, y - 1));
+//     }
+
+//     else 
+//     {
+//         return 0;
+//     }
+// }
+// System.Console.WriteLine("input number 1: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("input number 2: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int function = Akkerman(m, n);
+
+// System.Console.WriteLine(function);
+
+
+///// Задание 3
+
+
+
+
